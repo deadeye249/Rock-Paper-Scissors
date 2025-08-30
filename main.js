5596eeb45ee88c1f9ddeff0aa3907b9aeeb489b1
Muhammad Mahad <65706696+deadeye249@users.noreply.github.com>
2025-08-30 15:31:37 +0500

    Add Logic for Game Handling

--- main.js
+++ main.js
@@ -21,43 +21,65 @@ function getComputerChoice()
     }
 }
 
-let human_score = 0;
-let computer_score = 0;
 
-function playRound(humanChoice, computerChoice)
-{
-    let human_choice = humanChoice.toLowerCase();
-    if(human_choice == computerChoice){
-        return "It's a tie! Try again.";
+
+function playGame(){
+
+    let human_score = 0;
+    let computer_score = 0;
+
+    function playRound(humanChoice, computerChoice)
+    {
+        let human_choice = humanChoice.toLowerCase();
+        if(human_choice == computerChoice){
+            return "It's a tie! Try again.";
+        }
+        else if(human_choice == "rock" && computerChoice == "paper"){
+            computer_score+=1;
+            return "You lose! Paper beats Rock.";
+        }
+        else if(human_choice == "rock" && computerChoice == "scissors"){
+            human_score+=1;
+            return "You win! Rock beays Scissors.";
+        }
+        else if(human_choice == "paper" && computerChoice == "scissors"){
+            computer_score+=1;
+            return "You lose! Paper beats Scissors.";
+        }
+        else if(human_choice == "paper" && computerChoice == "rock"){
+            human_score+=1;
+            return "You win! Paper beats Rock.";
+        }
+        else if(human_choice == "scissors" && computerChoice == "rock"){
+            computer_score+=1;
+            return "You lose! Rock beats Scissors.";
+        }
+        else if(human_choice == "scissors" && computerChoice == "paper"){
+            human_score+=1;
+            return "You win! Scissors beats Paper.";
+        }
+        else{
+            return "Wrong Input";
+        }
+
     }
-    else if(human_choice == "rock" && computerChoice == "paper"){
-        computer_score+=1;
-        return "You lose! Paper beats Rock.";
+
+    for(let i=0;i<5;i++){
+        const humanSelection = getPlayerChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection,computerSelection);
     }
-    else if(human_choice == "rock" && computerChoice == "scissors"){
-        human_score+=1;
-        return "You win! Rock beays Scissors.";
+
+    if(human_score == computer_score){
+        return "The game is a tie!";
     }
-    else if(human_choice == "paper" && computerChoice == "scissors"){
-        computer_score+=1;
-        return "You lose! Paper beats Scissors.";
-    }
-    else if(human_choice == "paper" && computerChoice == "rock"){
-        human_score+=1;
-        return "You win! Paper beats Rock.";
-    }
-     else if(human_choice == "scissors" && computerChoice == "rock"){
-        computer_score+=1;
-        return "You lose! Rock beats Scissors.";
-    }
-     else if(human_choice == "scissors" && computerChoice == "paper"){
-        human_score+=1;
-        return "You win! Scissors beats Paper.";
+    else if(human_score>computer_score){
+        return "You win!";
     }
     else{
-        return "Wrong Input";
+        return "You lose!";
     }
-
-
 }
+
+playGame();
 
