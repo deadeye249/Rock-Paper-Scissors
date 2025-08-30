bf057d75702ce7de16fbddb02ca7c5e70010162d
Muhammad Mahad <65706696+deadeye249@users.noreply.github.com>
2025-08-30 15:23:33 +0500

    Add Logic for a Single Round of R-P-S

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-function getPlayerChoice(){
+function getPlayerChoice()
+{
     let choice = prompt("Please enter your choice: ");
     return choice;
 }
-console.log("Hello World")
 
-function getComputerChoice(){
+function getComputerChoice()
+{
     let choice = Math.random()*3;
     if(choice<=1)
     {
@@ -16,7 +17,47 @@ function getComputerChoice(){
     }
     else
     {
-        return "scissors"
+        return "scissors";
     }
+}
+
+let human_score = 0;
+let computer_score = 0;
+
+function playRound(humanChoice, computerChoice)
+{
+    let human_choice = humanChoice.toLowerCase();
+    if(human_choice == computerChoice){
+        return "It's a tie! Try again.";
+    }
+    else if(human_choice == "rock" && computerChoice == "paper"){
+        computer_score+=1;
+        return "You lose! Paper beats Rock.";
+    }
+    else if(human_choice == "rock" && computerChoice == "scissors"){
+        human_score+=1;
+        return "You win! Rock beays Scissors.";
+    }
+    else if(human_choice == "paper" && computerChoice == "scissors"){
+        computer_score+=1;
+        return "You lose! Paper beats Scissors.";
+    }
+    else if(human_choice == "paper" && computerChoice == "rock"){
+        human_score+=1;
+        return "You win! Paper beats Rock.";
+    }
+     else if(human_choice == "scissors" && computerChoice == "rock"){
+        computer_score+=1;
+        return "You lose! Rock beats Scissors.";
+    }
+     else if(human_choice == "scissors" && computerChoice == "paper"){
+        human_score+=1;
+        return "You win! Scissors beats Paper.";
+    }
+    else{
+        return "Wrong Input";
+    }
+
+
 }
 
