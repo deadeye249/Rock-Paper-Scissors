974507f540ceda7c07db3f9e8c320bfe384b7a20
Muhammad Mahad <65706696+deadeye249@users.noreply.github.com>
2025-08-30 14:33:17 +0500

    Add getComputerChoice Functionality in JS

---
+++ main.js
@@ -0,0 +1,18 @@
+console.log("Hello World")
+
+function getComputerChoice(){
+    let choice = Math.random()*3;
+    if(choice<=1)
+    {
+        return "rock";
+    }
+    else if(choice<=2)
+    {
+        return "paper";
+    }
+    else
+    {
+        return "scissors"
+    }
+}
+
